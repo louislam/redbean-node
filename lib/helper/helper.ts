25abdfbc01171a7043b1ddca508df32b70b1718b
LouisLam <louislam@users.noreply.github.com>
2021-08-23 10:29:56 +0800

    add SlowLogger class

--- lib/helper/helper.ts
+++ lib/helper/helper.ts
@@ -2,6 +2,8 @@
  * From: https://stackoverflow.com/questions/679915/how-do-i-test-for-an-empty-javascript-object
  * @param obj
  */
+import dayjs from "dayjs";
+
 export function isEmptyObject(obj : LooseObject) {
     for(var prop in obj) {
         if(obj.hasOwnProperty(prop)) {
@@ -14,4 +16,23 @@ export function isEmptyObject(obj : LooseObject) {
 
 export interface LooseObject<T = any> {
     [key: string]: T
+}
+
+export class SlowLogger {
+
+    static enable = false;
+    static threshold = 10000;
+
+    private startTime: any;
+    constructor() {
+        this.startTime = dayjs().valueOf();
+    }
+
+    log(sql) {
+        const time = (dayjs().valueOf() - this.startTime);
+
+        if (time >= SlowLogger.threshold) {
+            console.log(`[Slow Log][${time}ms] ${sql}`)
+        }
+    }
 }
